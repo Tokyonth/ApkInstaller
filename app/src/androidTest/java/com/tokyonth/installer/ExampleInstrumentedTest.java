1a18a497bfaea59ce8d6dc21631296a495cd1864
Tokyonth <1948226838@qq.com>
2020-06-02 20:51:55 +0800

    update

--- app/src/androidTest/java/com/tokyonth/installer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tokyonth/installer/ExampleInstrumentedTest.java
@@ -20,7 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.tokyonth.installer", appContext.getPackageName());
     }
 }
