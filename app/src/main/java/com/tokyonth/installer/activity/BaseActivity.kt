21b1b78208d6970b553fa39d8264bf86380de59c
Tokyonth <zcl...1224>
2021-09-30 18:02:11 +0800

    夜间模式状态栏，导航栏颜色调整

--- app/src/main/java/com/tokyonth/installer/activity/BaseActivity.kt
+++ app/src/main/java/com/tokyonth/installer/activity/BaseActivity.kt
@@ -5,18 +5,16 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.res.Configuration
-import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
-import android.view.View
-import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import androidx.viewbinding.ViewBinding
 import com.tokyonth.installer.App
 import com.tokyonth.installer.R
@@ -28,7 +26,10 @@ import com.tokyonth.installer.utils.NotificationUtil
 
 abstract class BaseActivity : AppCompatActivity() {
 
-    private val permissionArr = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    private val permissionArr = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
 
     abstract fun initView(): ViewBinding?
 
@@ -36,8 +37,19 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK).let {
+            val isNightMode = if (it == 0x20) {
+                window.navigationBarColor = ContextCompat.getColor(this, R.color.colorCard)
+                true
+            } else {
+                WindowCompat.getInsetsController(window, window.decorView)?.apply {
+                    isAppearanceLightStatusBars = true
+                }
+                false
+            }
+            App.localData.setNightMode(isNightMode)
+        }
         initView().let {
             if (it != null)
                 setContentView(it.root)
@@ -48,14 +60,14 @@ abstract class BaseActivity : AppCompatActivity() {
     private fun check() {
         if (App.localData.isFirstBoot()) {
             CustomizeDialog.getInstance(this)
-                    .setMessage(getString(R.string.use_app_warn))
-                    .setNegativeButton(getString(R.string.exit_app)) { _, _ -> finish() }
-                    .setPositiveButton(getString(R.string.dialog_btn_ok)) { _, _ ->
-                        requestPermission()
-                        NotificationUtil().checkNotification(this)
-                    }
-                    .setCancelable(false)
-                    .show()
+                .setMessage(getString(R.string.use_app_warn))
+                .setNegativeButton(getString(R.string.exit_app)) { _, _ -> finish() }
+                .setPositiveButton(getString(R.string.dialog_btn_ok)) { _, _ ->
+                    requestPermission()
+                    NotificationUtil().checkNotification(this)
+                }
+                .setCancelable(false)
+                .show()
         } else {
             requestPermission()
             NotificationUtil().checkNotification(this)
@@ -72,8 +84,15 @@ abstract class BaseActivity : AppCompatActivity() {
                 startActivityForResult(intent, Constants.PERM_CODE)
             }
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(this, permissionArr[0]) == PackageManager.PERMISSION_GRANTED &&
-                    ContextCompat.checkSelfPermission(this, permissionArr[1]) == PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permissionArr[0]
+                ) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    this,
+                    permissionArr[1]
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
                 initData()
             } else {
                 ActivityCompat.requestPermissions(this, permissionArr, Constants.PERM_CODE)
@@ -95,29 +114,15 @@ abstract class BaseActivity : AppCompatActivity() {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 
-    override fun onResume() {
-        super.onResume()
-        val currentNightMode = (resources.configuration.uiMode
-                and Configuration.UI_MODE_NIGHT_MASK)
-        if (currentNightMode == 32) {
-            window.navigationBarColor = ContextCompat.getColor(this, R.color.colorCard)
-            App.localData.setNightMode(true)
-        } else {
-            window.navigationBarColor = Color.TRANSPARENT
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
-            }
-            App.localData.setNightMode(false)
-        }
-    }
-
     @SuppressLint("WrongConstant")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == Constants.API_R_CODE) {
             data?.let {
-                contentResolver.takePersistableUriPermission(it.data!!, it.flags and (Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION))
+                contentResolver.takePersistableUriPermission(
+                    it.data!!,
+                    it.flags and (Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                )
                 initData()
             }
         }
@@ -136,11 +141,22 @@ abstract class BaseActivity : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == Constants.PERM_CODE) {
-            if (ActivityCompat.checkSelfPermission(this, permissionArr[0]) == PackageManager.PERMISSION_GRANTED &&
-                    ContextCompat.checkSelfPermission(this, permissionArr[1]) == PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permissionArr[0]
+                ) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    this,
+                    permissionArr[1]
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
                 initData()
             } else {
                 showToast(getString(R.string.no_permissions))
